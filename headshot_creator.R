8337796616070ec04d024141a6667f3d8b35b1ec
kaputkin <31864335+kaputkin@users.noreply.github.com>
2023-01-11 21:02:31 -0500

    Update headshot_creator.R

--- headshot_creator.R
+++ headshot_creator.R
@@ -1,23 +1,24 @@
-library(magick) 
-library(cropcircles)
+players <- read.csv('https://raw.githubusercontent.com/kaputkin/hit_distance_bot/main/player_list_2022.csv')
+ids <- players$batter
 
-names = c("Judge","Schwarber","Trout","Alonso","Riley", "Alvarez", "Walker", "Betts", "Tellez", "Goldschmidt")
 
-for (i in 1:length(names)) {
-  
+for (i in 1:length(ids)) {
+  #load packages
+  require(magick)
+  require(cropcircles)
   #Select and import CSV
-  player_name = names[i]
+  player_id = ids[i]
 
   image_write(
     image_composite(
-        image_read("C:/Users/kaputka/Downloads/ball_players/circle_bg.png"),
-        image_read(circle_crop(paste0("C:/Users/kaputka/Downloads/ball_players/", player_name, ".png"))),
+        image_read("C:/Users/kaputka/Downloads/ball_players/assets/circle_bg.png"),
+        image_read(circle_crop(paste0("C:/Users/kaputka/Downloads/ball_players/assets/headshots/", ids[i], ".jpg"))),
         operator = "atop",
         offset = "+0+0",
         gravity = "northwest",
         compose_args = ""
       ),
-    path = paste0("C:/Users/kaputka/Downloads/ball_players/crops/", player_name, ".png"),
+    path = paste0("C:/Users/kaputka/Downloads/ball_players/crops/", ids[i], ".png"),
     format = "png"
   )
 }
