647b7c862b087d1727c5686dac449f173c6033e8
Ari <arikaputkin@gmail.com>
2023-01-24 19:12:31 -0500

    updated circle

--- headshot_creator.R
+++ headshot_creator.R
@@ -1,5 +1,5 @@
 players <- read.csv('https://raw.githubusercontent.com/kaputkin/hit_distance_bot/main/player_list_2022.csv')
-ids <- players$batter
+batters <- players$batter
 
 
 for (i in 1:length(ids)) {
@@ -7,11 +7,11 @@ for (i in 1:length(ids)) {
   require(magick)
   require(cropcircles)
   #Select and import CSV
-  player_id = ids[i]
+  player_id = batters[i]
 
   image_write(
     image_composite(
-      image_read("https://raw.githubusercontent.com/kaputkin/hit_distance_bot/f0234e766d1001fb4e2d72e1c2d470db118d1ad8/assets/circle_bg.png"),
+      image_read("https://raw.githubusercontent.com/kaputkin/hit_distance_bot/main/assets/circle_bg_2.png"),
       image_read(circle_crop(paste0(tempdir(),'/',batters[i],'.png'))),
       operator = "atop",
       offset = "+0+0",
